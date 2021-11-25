244fb4173eae1ff0c022440eedafd4354fd7dbae
ozahirnyi <ozahirny@gmail.com>
2021-11-25 21:53:01 +0200

    change registration.html. But registration still doesnt work.

--- movie_finder/finder/views.py
+++ movie_finder/finder/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from .forms import CustomUserCreationForm
 from .models import Movie
 from django.contrib.auth.decorators import login_required
 
@@ -7,3 +9,15 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     movies = Movie.objects.filter()
     return render(request, 'finder/index.html', context={'movies': movies})
+
+
+def registration(request):
+    if request.POST == 'POST':
+        form = CustomUserCreationForm()
+        if form.is_valid():
+            form.save()
+            return render(request, 'finder/index.html')
+    else:
+        form = CustomUserCreationForm()
+    context = {'form': form}
+    return render(request, 'registration/registration.html', context)
--- movie_finder/movie_finder/urls.py
+++ movie_finder/movie_finder/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from finder.views import index
+from finder.views import index, registration
 from django.contrib.auth import views
 
 urlpatterns = [
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', index),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('registration/', registration, name='registration'),
     path('password-change/', views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('password-reset/', views.PasswordResetView.as_view(), name='password_reset'),
