c321a5dd25cf755b464f43bff3a82a3233c4d13f
ozahirnyi <ozahirny@gmail.com>
2021-12-06 18:50:47 +0200

    favorites works bad. Need to add js to index html for do not generating new tempalate after add to favorites

--- movie_finder/finder/admin.py
+++ movie_finder/finder/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Actor, Movie
+from .models import FavoriteMovieUser
 
-admin.register(Actor)(admin.ModelAdmin)
-admin.register(Movie)(admin.ModelAdmin)
+admin.register(FavoriteMovieUser)(admin.ModelAdmin)
--- movie_finder/finder/views.py
+++ movie_finder/finder/views.py
@@ -4,32 +4,59 @@ import requests
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .forms import CustomUserCreationForm
+from .models import FavoriteMovieUser
 
 
 @login_required(login_url=settings.LOGIN_PAGE_URL)
 def favorites(request):
-    return render(request, 'finder/favorites.html')
+    if request.method == 'GET':
+        favorite_movies = FavoriteMovieUser.objects.filter(
+                user=request.user).values()
+        return render(request, 'finder/favorites.html', context={
+            'favorite_movies': favorite_movies})
+    else:
+        return HttpResponse(status=501)
+
+
+def add_to_favorites(movie, user):
+    FavoriteMovieUser.objects.create(title=movie['Title'],
+                                     imdb_id=movie['imdbID'],
+                                     year=movie['Year'],
+                                     type=movie['Type'],
+                                     user=user)
+
+
+def find_movie(movie_to_find):
+    querystring = {'s': movie_to_find, 'r': 'json'}
+    headers = {
+        'x-rapidapi-host': settings.KOSTILNIE_VARIABLES[
+            'X_RAPIDAPI_HOST'],
+        'x-rapidapi-key': settings.KOSTILNIE_VARIABLES[
+            'X_RAPIDAPI_KEY']
+    }
+    response = json.loads(
+        requests.request("GET", settings.IMDB_API_URL, headers=headers,
+                         params=querystring).text)
+    context = {'movies': response['Search']} if response.get(
+        'Search') else {
+        'messages': ['There are no any info for your request']}
+    return context
 
 
 @login_required(login_url=settings.LOGIN_PAGE_URL)
 def index(request):
     if request.method == 'POST':
-        movie_to_find = request.POST['movie_name_to_find']
-        querystring = {"s": movie_to_find, "r": "json"}
-        headers = {
-            'x-rapidapi-host': settings.KOSTILNIE_VARIABLES[
-                'X_RAPIDAPI_HOST'],
-            'x-rapidapi-key': settings.KOSTILNIE_VARIABLES[
-                'X_RAPIDAPI_KEY']
-        }
-        response = json.loads(requests.request("GET", settings.IMDB_API_URL, headers=headers,
-                                               params=querystring).text)
-        context = {'movies': response['Search']} if response.get('Search') else {
-            'messages': ['There are no any info for your request']}
-        return render(request, 'finder/index.html', context=context)
+        movie_to_find = request.POST.get('movie_name_to_find')
+        movie_to_favorites = request.POST.get('movie_to_favorites')
+        if movie_to_find:
+            context = find_movie(movie_to_find)
+            return render(request, 'finder/index.html', context=context)
+        if movie_to_favorites:
+            add_to_favorites(eval(movie_to_favorites), request.user)
     return render(request, 'finder/index.html')
 
 
