90eba7141b5056de98683bfb9c00299dd5686949
andrii <andrewmagalyas@gmail.com>
2025-01-14 01:21:41 +0100

    comments resolving

--- movie_finder_django/movie_finder_django/settings.py
+++ movie_finder_django/movie_finder_django/settings.py
@@ -15,7 +15,7 @@ IMDB_API_URL = "https://api.collectapi.com/imdb/imdbSearchByName"
 IMDB_API_KEY = os.getenv("IMDB_API_KEY")
 
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
-MAX_PROMPT_TOKENS_LENGTH = 100
+MAX_PROMPT_TOKENS_LENGTH = 1000
 
 SECRET_KEY = os.getenv("DJANGO_KEY")
 
